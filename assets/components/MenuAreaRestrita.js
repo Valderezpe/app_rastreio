13b1978a77905f827097f5a7998059e1c415b423
valderez <valderezpe@gmail.com>
2025-05-11 20:41:02 -0300

    feat: add area restrita

---
+++ assets/components/MenuAreaRestrita.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import { css } from "../../assets/css/css";
+import Icon from "react-native-vector-icons/FontAwesome";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export default function MenuAreaRestrita(props) {
+  async function logout() {
+    await AsyncStorage.clear();
+    props.navigation.navigate("Login");
+  }
+
+  return (
+    <View style={css.area__menu}>
+      <TouchableOpacity
+        style={css.button__home2}
+        onPress={() => props.navigation.navigate("Home")}
+      >
+        <Icon name="home" size={25} color="#999" />
+      </TouchableOpacity>
+
+      <Text style={css.area__title}>{props.title} </Text>
+
+      <TouchableOpacity style={css.button__logout} onPress={() => logout()}>
+        <Icon name="sign-out" size={25} color="#999" />
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ assets/css/css.js
@@ -0,0 +1,97 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const css = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  containerTop: {
+    justifyContent: "flex-start",
+  },
+
+  containerImg: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  button__home: {
+    marginRight: 10,
+  },
+
+  darkbg: {
+    backgroundColor: "#333",
+  },
+
+  login__logomarca: {
+    marginBottom: 10,
+  },
+
+  login__msg: (text = "none") => ({
+    fontWeight: "bold",
+    fontSize: 22,
+    color: "red",
+    marginTop: 5,
+    marginBottom: 15,
+    display: text,
+  }),
+
+  login__form: {
+    width: "80%",
+  },
+
+  login__input: {
+    backgroundColor: "#fff",
+    fontSize: 19,
+    padding: 7,
+    marginBottom: 15,
+  },
+
+  login__button: {
+    padding: 12,
+    backgroundColor: "#F58634",
+    alignSelf: "center",
+    borderRadius: 5,
+  },
+
+  login__buttonText: {
+    fontWeight: "bold",
+    fontSize: 22,
+    color: "#333",
+  },
+  area__tab: {
+    backgroundColor: "#333",
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  area__menu: {
+    flexDirection: "row",
+    paddingTop: 40,
+    paddingBottom: 10,
+    width: "100%",
+    backgroundColor: "#111",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button__home2: {
+    textAlign: "left",
+  },
+  area__title: {
+    width: "80%",
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "#fff",
+    textAlign: "center",
+  },
+  button__logout: {
+    textAlign: "right",
+  },
+});
+
+export { css };
