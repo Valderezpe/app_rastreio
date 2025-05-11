91e674945c6948d7b8a23b10b7a94c5fe3e71a48
valderez <valderezpe@gmail.com>
2025-05-11 20:39:05 -0300

    feat: add initial project

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Home, Login, Rastreio } from "./views";
+import AreaRestrita from "./views/arearestrita/AreaRestrita";
+
+export default function App() {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: "QA-Express",
+            headerStyle: { backgroundColor: "#F58634" },
+            headerTintColor: "#333",
+            headerTitleStyle: { fontWeight: "bold", alignSelf: "center" },
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          options={{ headerShown: false }}
+          component={Login}
+        />
+        <Stack.Screen name="Rastreio" component={Rastreio} />
+        <Stack.Screen
+          name="AreaRestrita"
+          options={{
+            headerShown: false,
+          }}
+          component={AreaRestrita}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
